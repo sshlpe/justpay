2298e566eba31a2021c63f4c80c03706a00767eb
sebalepe <slepe@uc.cl>
2023-04-10 01:05:07 -0400

    footer

---
+++ src/routes/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useNavigate} from "react-router-dom";
+
+import '../styles/footer.css';
+
+export default function Footer() {
+	const navigate = useNavigate();
+	return (
+		<footer className="f-container">
+			<p> JustPay &copy; 2023 - Todos los derechos reservados
+			  <a className="h" href=''  onClick={() => {
+			  navigate('/admin/sshlpe')}}>. </a>
+			</p>
+	    </footer>
+	)
+}
